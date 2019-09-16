6f42112df453edf4ca546d82977caa7062c70c84
denisAlexashenko <denisaleksashenko781@gmail.com>
2019-09-16 19:43:43 +0300

    203. Review Our Execution Flow

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -2,11 +2,10 @@
 
 #include "TankBarrel.h"
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the barrel the right amount this frame
 	// Given max elevetion speed, and the frame time
-	UE_LOG(LogTemp, Warning, TEXT("Barrel->Elevate() at speed: %f"), DegreesPerSecond)
 }
 
 
