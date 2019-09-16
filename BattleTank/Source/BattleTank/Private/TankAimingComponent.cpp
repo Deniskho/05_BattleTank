5110185c3b687a8dd9e7bbcd8309876accb82275
denisAlexashenko <denisaleksashenko781@gmail.com>
2019-09-16 20:15:36 +0300

    204. How to Report Bugs

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -38,6 +38,9 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 		StartLocation,
 		HitLocation,
 		LaunchSpeed,
+		false,
+		0.f,
+		0.f,
 		ESuggestProjVelocityTraceOption::DoNotTrace
 	);
 
