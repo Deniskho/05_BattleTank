17f43c8cfe11374bae9a96596c1548fa52346058
denisAlexashenko <denisaleksashenko781@gmail.com>
2019-09-19 11:33:15 +0300

    221. Using BlueprintReadOnly

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -6,6 +6,7 @@
 #include "Engine/World.h"
 #include "TankBarrel.h"
 #include "Projectile.h"
+#include "TankMovementComponent.h"
 
 // Sets default values
 ATank::ATank()
@@ -15,6 +16,7 @@ ATank::ATank()
 
 	// No need to protect pointers as added at construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 void ATank::SetBarrelReference(UTankBarrel * BarrelToSet)
--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,6 +2,11 @@
 
 #include "TankMovementComponent.h"
 
-
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+	/*auto Time = GetWorld()->GetTimeSeconds();*/
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+}
 
 
