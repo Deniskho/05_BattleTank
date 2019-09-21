fc95081811d71040fbd60ad1eb9259e3c8eca8df
denisAlexashenko <denisaleksashenko781@gmail.com>
2019-09-21 21:20:06 +0300

    248. Tweaking Tank AI

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -14,11 +14,15 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+protected:
+	// How close can the AI tank get
+	UPROPERTY(EditDefaultsOnly, Category = "Setup") 
+	float AcceptanceRadius = 80000;
+
 public:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
 
-	// How close can the AI tank get
-	float AcceptanceRadius = 3000;
+
 };
